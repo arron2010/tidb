3de293eca7838b44f903c167e70679b76cc8d825
xiaopeng <arronxiao@126.com>
2020-09-28 17:22:46 +0800

    初始化

---
+++ memdb/buntdb_test.go
@@ -0,0 +1,240 @@
+package buntdb
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/tidwall/gjson"
+	"strconv"
+	"testing"
+	"time"
+)
+
+type Timer struct {
+	begin time.Time
+	end   time.Time
+}
+
+func NewTimer() *Timer {
+	t := &Timer{}
+	t.Start()
+	return t
+}
+func (t *Timer) Start() {
+	t.begin = time.Now()
+}
+
+func (t *Timer) Stop() float64 {
+	elapsed := time.Since(t.begin)
+	return elapsed.Seconds() * 1000
+}
+
+func TestIndexString(t *testing.T) {
+	db, _ := Open(":memory:")
+	//db.CreateIndex("name", "*", IndexInt)
+	db.Update(func(tx *Tx) error {
+		tx.Set("8", "Tom", nil)
+		tx.Set("2", "Janet", nil)
+		tx.Set("3", "Carol", nil)
+		tx.Set("4", "Alan", nil)
+		tx.Set("5", "Sam", nil)
+		tx.Set("9", "Melinda", nil)
+		return nil
+	})
+
+	db.View(func(tx *Tx) error {
+		tx.AscendRange("name", "5", "8", func(key, value string) bool {
+			fmt.Printf("%s: %s\n", key, value)
+			return true
+		})
+		return nil
+	})
+
+	//db.View(func(tx *Tx) error {
+	//	tx.Ascend("name", func(key, value string) bool {
+	//		fmt.Printf("%s: %s\n", key, value)
+	//		return true
+	//	})
+	//	return nil
+	//})
+
+}
+
+type Dimension struct {
+	Dim1  uint64
+	Dim2  string
+	Dim3  string
+	Dim4  string
+	Dim5  string
+	Value float64
+}
+
+func IndexJSON2(path string) func(a, b string) bool {
+	return func(a, b string) bool {
+		v1 := gjson.Get(a, path)
+		v2 := gjson.Get(b, path)
+		if v1.Str == v2.Str {
+			return true
+		} else {
+			return false
+		}
+		r := gjson.Get(a, path).Less(gjson.Get(b, path), true)
+		return r
+	}
+}
+
+var gDB *DB
+
+func testLoadData(t *testing.T) {
+	//a := ","
+	//b := "AAA1,AAA2,"
+	//index := strings.Index(b,"AAA1,")
+	//fmt.Println(b[index])
+	gDB, _ = Open(":memory:")
+	const NUM = 10 * 10000
+	gDB.CreateIndex("Dim2_Dim3", "*", IndexJSON("Dim2"))
+	for i := 1; i <= NUM; i++ {
+		k := uint64(i)
+		item := &Dimension{}
+		item.Dim1 = k
+		item.Dim2 = "AAA" + strconv.Itoa(i)
+
+		item.Dim3 = "BBB" + strconv.Itoa(i)
+		item.Dim4 = "CCC" + strconv.Itoa(i)
+		item.Dim5 = "DDD" + strconv.Itoa(i)
+		item.Value = float64(k)
+
+		val, _ := json.Marshal(item)
+		strVal := string(val)
+		strKey := strconv.Itoa(i)
+		gDB.Update(func(tx *Tx) error {
+			tx.Set(strKey, strVal, nil)
+			return nil
+		})
+	}
+	//gDB.Update(func(tx *Tx) error {
+	//	tx.SetHitHandler("Dim2", func(scope map[interface{}]bool, item string) bool {
+	//		//token :=gjson.Get(item,"Dim2")
+	//		//val := token.Str
+	//		//if len(val) ==0{
+	//		//	return false
+	//		//}
+	//		_,ok := scope[item]
+	//		return ok
+	//	})
+	//	return nil
+	//})
+	//val,_ := json.Marshal(items)
+	//r := gjson.ParseBytes(val)
+	//fmt.Println(r)
+}
+
+func TestDB_Indexes01(t *testing.T) {
+	//testLoadData(t)
+	//var err error
+	//timer := NewTimer()
+	////var result string
+	//
+	//	err = gDB.View(func(tx *Tx) error {
+	//		err :=tx.SelectRange("Dim2","1,2,3,4",func(key, value string) bool {
+	//			//token := gjson.Get(value,"Dim2")
+	//			//if token.Str == "AAA9999"{
+	//			//fmt.Printf("%s: %s\n", key, value)
+	//			//}
+	//
+	//			return true
+	//		})
+	//		if err != nil{
+	//			fmt.Println(err)
+	//		}
+	//		return nil
+	//	})
+	//	if err != nil{
+	//		fmt.Println(err)
+	//	}
+	//
+	//
+	//fmt.Println(timer.Stop())
+
+}
+func TestDB_Indexes04(t *testing.T) {
+	testLoadData(t)
+	var err error
+	timer := NewTimer()
+	//var result string
+	const NUM = 1
+	//for i :=1;i <= NUM;i++{
+	val := `{"Dim2":"AAA1","Dim3":"BBB1"}`
+	err = gDB.View(func(tx *Tx) error {
+		err := tx.AscendEqual("Dim2_Dim3", val, func(key, value string) bool {
+
+			fmt.Printf("%s: %s\n", key, value)
+
+			return true
+		})
+		if err != nil {
+			fmt.Println(err)
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+	//}
+	fmt.Println(timer.Stop())
+
+}
+
+func TestDB_Indexes05(t *testing.T) {
+	testLoadData(t)
+	var err error
+	timer := NewTimer()
+	//var result string
+	var count int
+	const NUM = 1 * 100
+	for i := 1; i <= NUM; i++ {
+		val := fmt.Sprintf(`{"Dim2":"AAA%d"}`, i)
+		err = gDB.View(func(tx *Tx) error {
+			err := tx.AscendEqual("Dim2_Dim3", val, func(key, value string) bool {
+
+				count++
+				//fmt.Printf("%s: %s\n", key, value)
+				return true
+			})
+			if err != nil {
+				fmt.Println(err)
+			}
+			return nil
+		})
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	fmt.Println("时间花费:", timer.Stop())
+	fmt.Println("记录数量:", count)
+}
+
+func TestDB_Indexes03(t *testing.T) {
+	db, _ := Open(":memory:")
+	db.CreateIndex("last_name", "*", IndexJSON("name.last"))
+	db.CreateIndex("age", "*", IndexJSON("age"))
+	db.Update(func(tx *Tx) error {
+		tx.Set("1", `{"name":{"first":"Tom","last":"Johnson"},"age":38}`, nil)
+		tx.Set("2", `{"name":{"first":"Janet","last":"Johnson"},"age":47}`, nil)
+		tx.Set("3", `{"name":{"first":"Carol","last":"Anderson"},"age":52}`, nil)
+		tx.Set("4", `{"name":{"first":"Alan","last":"Cooper"},"age":28}`, nil)
+		return nil
+	})
+	db.View(func(tx *Tx) error {
+		fmt.Println("Order by last name")
+		tx.AscendEqual("last_name", `{"name":{"first":"Tom","last":"Johnson"},"age":38}`, func(key, value string) bool {
+			fmt.Printf("%s: %s\n", key, value)
+			return true
+		})
+
+		//tx.Ascend("last_name", func(key, value string) bool {
+		//	fmt.Printf("%s: %s\n", key, value)
+		//	return true
+		//})
+		return nil
+	})
+}
